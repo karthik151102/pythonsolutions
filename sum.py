8b445edc488ed95d60735974f86d472ad80882ba
Karthik Goud <kgoud9991@gmail.com>
2024-12-23 21:54:55 +0530

    addition of varibles

---
+++ sum.py
@@ -0,0 +1,7 @@
+def calc_sum(a, b):
+    return a + b
+# Test the function with some values
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = calc_sum(a,b)
+print("Sum is", c)
